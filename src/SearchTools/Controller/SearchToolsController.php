7f0269a5ebbf7f1227782bdcb3171f4c0ddc919d
Oliver Stöhr <stoehr@effective-webwork.de>
2024-10-29 14:57:22 +0100

    Add code

---
+++ src/SearchTools/Controller/SearchToolsController.php
@@ -0,0 +1,26 @@
+<?php
+namespace SearchTools\Controller;
+
+/**
+ * Feedback Class
+ *
+ * Controls the SearchTools
+ *
+ * @category VuFind
+ * @package  Controller
+ * @author   Kristof Keßler <kristof.kessler@tu-braunschweig.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+class SearchToolsController extends \VuFind\Controller\AbstractBase
+{
+    /**
+     * Display Feedback home form.
+     *
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function homeAction()
+    {
+        return $this->createViewModel(['template' => $this->params()->fromQuery('template')]);
+    }
+}
